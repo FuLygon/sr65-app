af200f1e3264e40f441582027e524a48ce7787b0
Thanh Thai <thanhthai@fulygon.com>
2024-06-03 17:18:54 +0700

    feat(test): add test for ConvertDynamic()
    Signed-off-by: Thanh Thai <thanhthai@fulygon.com>

--- internal/converter_test.go
+++ internal/converter_test.go
@@ -1,8 +1,10 @@
 package internal
 
 import (
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"image"
 	"image/color"
+	"image/gif"
 	"image/png"
 	"os"
 	"path/filepath"
@@ -11,8 +13,11 @@ import (
 
 const (
 	testImage           = "test.png"
+	testGif             = "test.gif"
+	testVideo           = "test.mp4"
 	outputDir           = "outputs_test"
 	outputStaticExt     = "jpg"
+	outputDynamicExt    = "mjpeg"
 	outputStaticQuality = 95
 )
 
@@ -26,17 +31,17 @@ func TestConvertStatic(t *testing.T) {
 	}
 
 	// create test file
-	f, err := os.Create(testImage)
+	testFile, err := os.Create(testImage)
 	if err != nil {
 		panic(err)
 	}
 	defer func() {
-		f.Close()
-		os.Remove(f.Name())
+		testFile.Close()
+		os.Remove(testFile.Name())
 	}()
 
 	// encode test image
-	if err = png.Encode(f, img); err != nil {
+	if err = png.Encode(testFile, img); err != nil {
 		panic(err)
 	}
 
@@ -47,10 +52,10 @@ func TestConvertStatic(t *testing.T) {
 	}
 
 	// convert static media
-	ConvertStatic(f.Name(), outputDir, outputStaticExt, outputStaticQuality)
+	ConvertStatic(testFile.Name(), outputDir, outputStaticExt, outputStaticQuality)
 
 	// check if the output file exist
-	outputPath := generateOutput(f.Name(), outputDir, outputStaticExt)
+	outputPath := generateOutput(testFile.Name(), outputDir, outputStaticExt)
 	if _, err = os.Stat(outputPath); os.IsNotExist(err) {
 		t.Errorf("output file was not created")
 	}
@@ -75,7 +80,119 @@ func TestConvertStatic(t *testing.T) {
 	}
 }
 
-func TestGenerateOutputStatic(t *testing.T) {
+func TestConvertDynamicGif(t *testing.T) {
+	// prepare test file
+	img := image.NewRGBA(image.Rect(0, 0, 512, 512))
+	for y := 0; y < img.Bounds().Dy(); y++ {
+		for x := 0; x < img.Bounds().Dx(); x++ {
+			img.Set(x, y, color.RGBA{})
+		}
+	}
+
+	// create test file
+	testFile, err := os.Create(testGif)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		testFile.Close()
+		os.Remove(testFile.Name())
+	}()
+
+	// encode test gif
+	if err = gif.Encode(testFile, img, nil); err != nil {
+		panic(err)
+	}
+
+	// create output directory
+	err = os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	// convert gif
+	ConvertDynamic(testFile.Name(), outputDir, outputDynamicExt)
+
+	// check if the output file exist
+	outputPath := generateOutput(testVideo, outputDir, outputDynamicExt)
+	if _, err = os.Stat(outputPath); os.IsNotExist(err) {
+		t.Errorf("output file was not created")
+	}
+	defer os.RemoveAll(outputDir)
+
+	// open output file
+	outputFile, err := os.Open(outputPath)
+	if err != nil {
+		t.Errorf("error opening output file: %v", err)
+	}
+	defer outputFile.Close()
+
+	// decode output file
+	config, _, err := image.DecodeConfig(outputFile)
+	if err != nil {
+		t.Errorf("error decoding output file: %v", err)
+	}
+
+	// check output file dimensions
+	if config.Width != 128 || config.Height != 128 {
+		t.Errorf("output gif dimensions is incorrect, got: %dx%d, want: 128x128", config.Width, config.Height)
+	}
+}
+
+func TestConvertDynamicVideo(t *testing.T) {
+	// generate blank video
+	err := ffmpeg.Input("color=c=black:s=512x512:d=5", ffmpeg.KwArgs{"f": "lavfi"}).
+		Output(testVideo, ffmpeg.KwArgs{"c:v": "libx264", "pix_fmt": "yuv420p", "t": "5"}).
+		OverWriteOutput().
+		Silent(ffmpegSilent).
+		Run()
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(testVideo)
+
+	// get test file path
+	testVideoPath, err := filepath.Abs(testVideo)
+	if err != nil {
+		panic(err)
+	}
+
+	// create output directory
+	err = os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	// convert mp4
+	ConvertDynamic(testVideoPath, outputDir, outputDynamicExt)
+
+	// check if the output file exist
+	outputPath := generateOutput(testVideo, outputDir, outputDynamicExt)
+	if _, err = os.Stat(outputPath); os.IsNotExist(err) {
+		t.Errorf("output file was not created")
+	}
+	defer os.RemoveAll(outputDir)
+
+	// open output file
+	outputFile, err := os.Open(outputPath)
+	if err != nil {
+		t.Errorf("error opening output file: %v", err)
+	}
+	defer outputFile.Close()
+
+	// decode output file
+	config, _, err := image.DecodeConfig(outputFile)
+	if err != nil {
+		t.Errorf("error decoding output file: %v", err)
+	}
+
+	// check output file dimensions
+	if config.Width != 128 || config.Height != 128 {
+		t.Errorf("output video dimensions is incorrect, got: %dx%d, want: 128x128", config.Width, config.Height)
+	}
+}
+
+func TestGenerateOutput(t *testing.T) {
 	outputPath := generateOutput(testImage, outputDir, outputStaticExt)
 
 	// check output path
